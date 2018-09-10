cababcd4de0b900f849d679817c6caea8d306970
Qishi Zheng <vincezheng4265@gmail.com>
2018-09-10 23:56:59 +1000

    added agent class, waiting to be implemented;

---
+++ src/solver/Agent.java
@@ -0,0 +1,14 @@
+package solver;
+
+import problem.ProblemSpec;
+
+/**
+ * The search agent
+ */
+public class Agent {
+    private ProblemSpec ps;
+
+    public void agent(ProblemSpec problem) {
+        ps = problem;
+    }
+}
