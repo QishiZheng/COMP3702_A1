e3a0bd29632a217267accfd6c1bf0e65818b74c3
Qishi Zheng <vincezheng4265@gmail.com>
2018-09-17 06:43:54 +1000

    combined the path of robot and boxes;

--- src/solver/Agent.java
+++ src/solver/Agent.java
@@ -28,83 +28,149 @@ public class Agent {
 
         ProblemSpec ps = new ProblemSpec();
         try {
-            ps.loadProblem("input1.txt");
+            ps.loadProblem("input2.txt");
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         //create a initial state from the problrmSpec
         State s = new State(ps);
+        List<State> path = new LinkedList<>();
         //the length of robot
         Double robotLength = ps.getRobotWidth();
-
-
-        //TODO: GET THE PAHT OF MOVINGBOXES
-//        Astar astar = new Astar();
-
-
-
-
-
-
-        //TODO: TO BE REMOVED LATER,  KEEPING IT NOW ONLY FOR TESTING PURPOSE
-        //get the initial robot config from problemSpec
+        //get the initial and goal robot config from problemSpec
         RobotConfig robotInit = ps.getInitialRobotConfig();
-        System.out.println("Root robotConf: (" +robotInit.getPos().getX() + ", "
-                + robotInit.getPos().getY() + ", " + robotInit.getOrientation() + ")");
 
+        //GET THE PAHT OF MOVINGBOXES states
+        Astar astar = new Astar(ps);
+        List<LinkedList<State>> boxStates= astar.getStateList();
 
-        RobotConfig robotGoal = new RobotConfig(new Point2D.Double(ps.getMovingBoxes().get(1).getPos().getX() + robotLength, ps.getMovingBoxes().get(1).getPos().getY()  + robotLength/2), Math.PI/2);
-        if(!s.robotCollisionFree(robotGoal)) {
-            System.out.println("Goal in obstacle\n" + "***********************");
-        }
-
-        System.out.println("Goal robotConf: (" +robotGoal.getPos().getX() + ", "
-                + robotGoal.getPos().getY() + ", " + robotGoal.getOrientation() + ")");
+        RobotConfig robotGoal = boxStates.get(0).getFirst().getRobot();
+        System.out.println(robotInit);
+        System.out.println(robotGoal);
 
         PRM prm = new PRM(ps, s, 500, 10, robotInit, robotGoal);
-//        prm.setInit(robotInit);
-//        prm.setGoal(robotGoal);
-
-        //StateGraph sg = prm.buildGraph();
         HashMap<RobotConfig, Set<RobotConfig>> sg = prm.buildMap();
-//        System.out.println("#Vertex: "+sg.numOfVertex());
-        System.out.println("#Vertex: "+sg.keySet().size());
-
-
-        long consTime = System.currentTimeMillis();
-        System.out.println("Graph Construction Took "+(consTime - startTime)/1000 + " s");
-
-        System.out.println("Num of edges that Root vertex has: " + sg.get(prm.getInit()).size());
-        System.out.println("Num of edges that Goal vertex has: " + sg.get(prm.getGoal()).size());
 
         //check if the path exists
         if(prm.BFS(sg, robotInit,robotGoal) != null) {
-            System.out.println("Path for robotGoal:\n");
-            List<State> path = prm.BFS(sg,robotInit,robotGoal);
+            path = prm.BFS(sg,robotInit,robotGoal);
+        } else {
+            System.out.println("No Solution from init to 1st box");
+        }
 
-            //time taken
-            long searchTime = System.currentTimeMillis();
-            System.out.println("Search Time Took "+(searchTime - consTime) + " ms");
+        // the state list of boxes
+        if (boxStates.size()  == 1 ) {
+            path.addAll(boxStates.get(0));
+        } else {
+            for (int i = 0; i < boxStates.size() - 1; i++) {
+                //add state path of ith box
+                path.addAll(boxStates.get(i));
 
-//            for(int i = 0; i < path.size(); i++) {
-//            State state = path.get(i);
-//            System.out.println(i + " State: (" + state.toString() + ")");
-//            }
+//            //calculate the path from the box to next
+//            robotInit = boxStates.get(i).getLast().getRobot();
+//            robotGoal = boxStates.get(i + 1).getFirst().getRobot();
+//            System.out.println(robotInit);
+//            System.out.println(robotGoal);
+                PRM prm2 = new PRM(ps, boxStates.get(i).getLast(), 500, 10, boxStates.get(i).getLast().getRobot(), boxStates.get(i + 1).getFirst().getRobot());
+                HashMap<RobotConfig, Set<RobotConfig>> roadmap = prm2.buildMap();
 
-            SolutionWriter writer = new SolutionWriter();
+                //check if the path exists
+                if(prm2.BFS(roadmap, robotInit,robotGoal) != null) {
+                    path.addAll(prm2.BFS(roadmap,robotInit,robotGoal));
+                } else {
+                    System.out.println("No Solution from " + i + "th to"+ i+1+"th box");
+                }
+            }
+
+        }
+
+        SolutionWriter writer = new SolutionWriter();
             try {
                 writer.writePath(path, "solution1.txt");
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
-            long writeTime = System.currentTimeMillis();
-            System.out.println("Writing Time Took "+(writeTime - searchTime) + " ms");
 
-        } else {
-            System.out.println("No Solution");
-        }
+
+
+
+
+
+
+
+////        prm.setInit(robotInit);
+////        prm.setGoal(robotGoal);
+//
+//            //StateGraph sg = prm.buildGraph();
+//            HashMap<RobotConfig, Set<RobotConfig>> sg = prm.buildMap();
+
+
+
+
+
+
+
+
+//        //TODO: TO BE REMOVED LATER,  KEEPING IT NOW ONLY FOR TESTING PURPOSE
+//        //get the initial robot config from problemSpec
+//        //RobotConfig robotInit = ps.getInitialRobotConfig();
+//        System.out.println("Root robotConf: (" +robotInit.getPos().getX() + ", "
+//                + robotInit.getPos().getY() + ", " + robotInit.getOrientation() + ")");
+//
+//
+//        RobotConfig robotGoal = new RobotConfig(new Point2D.Double(ps.getMovingBoxes().get(1).getPos().getX() + robotLength, ps.getMovingBoxes().get(1).getPos().getY()  + robotLength/2), Math.PI/2);
+//        if(!s.robotCollisionFree(robotGoal)) {
+//            System.out.println("Goal in obstacle\n" + "***********************");
+//        }
+//
+//        System.out.println("Goal robotConf: (" +robotGoal.getPos().getX() + ", "
+//                + robotGoal.getPos().getY() + ", " + robotGoal.getOrientation() + ")");
+//
+//        PRM prm = new PRM(ps, s, 500, 10, robotInit, robotGoal);
+////        prm.setInit(robotInit);
+////        prm.setGoal(robotGoal);
+//
+//        //StateGraph sg = prm.buildGraph();
+//        HashMap<RobotConfig, Set<RobotConfig>> sg = prm.buildMap();
+////        System.out.println("#Vertex: "+sg.numOfVertex());
+//        System.out.println("#Vertex: "+sg.keySet().size());
+//
+//
+//        long consTime = System.currentTimeMillis();
+//        System.out.println("Graph Construction Took "+(consTime - startTime)/1000 + " s");
+//
+//        System.out.println("Num of edges that Root vertex has: " + sg.get(prm.getInit()).size());
+//        System.out.println("Num of edges that Goal vertex has: " + sg.get(prm.getGoal()).size());
+//
+//        //check if the path exists
+//        if(prm.BFS(sg, robotInit,robotGoal) != null) {
+//            System.out.println("Path for robotGoal:\n");
+//            path = prm.BFS(sg,robotInit,robotGoal);
+//
+//            //time taken
+//            long searchTime = System.currentTimeMillis();
+//            System.out.println("Search Time Took "+(searchTime - consTime) + " ms");
+//
+////            for(int i = 0; i < path.size(); i++) {
+////            State state = path.get(i);
+////            System.out.println(i + " State: (" + state.toString() + ")");
+////            }
+//
+//            SolutionWriter writer = new SolutionWriter();
+//            try {
+//                writer.writePath(path, "solution1.txt");
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//
+//            long writeTime = System.currentTimeMillis();
+//            System.out.println("Writing Time Took "+(writeTime - searchTime) + " ms");
+//
+//        } else {
+//            System.out.println("No Solution");
+//        }
 
 
 
