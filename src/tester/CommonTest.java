85252e3ba2af105f3dc4628388e897ccb411e8c6
Qishi Zheng <vincezheng4265@gmail.com>
2018-09-17 02:11:54 +1000

    implemented solutionWriter, it writes the path of state in the standard format of output file;

--- src/tester/CommonTest.java
+++ src/tester/CommonTest.java
@@ -1,5 +1,24 @@
 package tester;
 
+import problem.ProblemSpec;
+import solver.State;
+
+import java.io.IOException;
+
 public class CommonTest {
+    public static void main(String[] args) {
+        ProblemSpec ps = new ProblemSpec();
+        try {
+            ps.loadProblem("input1.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //create a initial state from the problrmSpec
+        State s = new State(ps);
+        System.out.println(s);
+    }
+
+
 
 }
